d08b5c6f4fc759f6c7f54749eb76a77b99efee68
renaldi <renalazyy@gmail.com>
2019-10-15 11:44:40 +0700

    initial changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/renaldi/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/renaldi/Android/tab_bar"
-export "FLUTTER_TARGET=/Users/renaldi/Android/tab_bar/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/renaldi/Flutter/jdih-apps"
+export "FLUTTER_TARGET=/Users/renaldi/Flutter/jdih-apps/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/renaldi/Development/flutter/bin/cache/artifacts/engine/ios"
